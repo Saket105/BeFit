04229e01d02d36162bbcdd84978fb79b7fe2a755
Saket105 <57018899+Saket105@users.noreply.github.com>
2020-12-12 22:04:43 +0530

    fourth Commit

--- app/src/main/java/com/example/befit/Activities/LoginActivity.java
+++ app/src/main/java/com/example/befit/Activities/LoginActivity.java
@@ -1,18 +1,124 @@
 package com.example.befit.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.befit.Animation.Anim;
+import com.example.befit.MainActivity;
 import com.example.befit.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
+    Button signIn;
+    TextView reg_page;
+    FirebaseAuth mAuth;
+    DatabaseReference reference;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.sign_email);
+        password = findViewById(R.id.sign_password);
+        signIn = findViewById(R.id.signIn_user);
+        reg_page = findViewById(R.id.tv);
+        mAuth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        reg_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+                        Anim.animateFade(LoginActivity.this);
+                    }
+                },500);
+            }
+        });
+    }
+
+    private void loginUser() {
+        String emails, pwd;
+        emails = email.getText().toString();
+        pwd = password.getText().toString();
+
+        if (emails.isEmpty()){
+            email.setError("Required!");
+            email.requestFocus();
+            return;
+
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(emails).matches()){
+            email.setError("Please provide valid email address");
+            email.requestFocus();
+            return;
+
+        }else if (pwd.length() < 6){
+            password.setError("Please provide valid password");
+            password.requestFocus();
+            return;
+
+        }else if (pwd.isEmpty()){
+            password.setError("Required!");
+            password.requestFocus();
+            return;
+
+        }else {
+            logUser(emails,pwd);
+        }
+    }
+
+    private void logUser(String emails, String pwd) {
+        mAuth.signInWithEmailAndPassword(emails, pwd)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this,"Ok! You are Logged In", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/befit/Animation/Anim.java
+++ app/src/main/java/com/example/befit/Animation/Anim.java
@@ -9,4 +9,18 @@ public class Anim {
     public static void animateSlideUp(Context context) {
         ((Activity) context).overridePendingTransition(R.anim.slide_up,R.anim.slide_up_exit);
     }
+
+    public static void animateZoom(Context context) {
+        ((Activity) context).overridePendingTransition(R.anim.animate_zoom_enter, R.anim.animate_zoom_exit);
+    }
+
+    public static void animateFade(Context context) {
+        ((Activity) context).overridePendingTransition(R.anim.animate_fade_enter, R.anim.animate_fade_exit);
+    }
+
+    public static void animateCard(Context context) {
+        ((Activity) context).overridePendingTransition(R.anim.animate_card_enter, R.anim.animate_card_exit);
+    }
+
+
 }
