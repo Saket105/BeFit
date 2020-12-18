545717a5680dbad03b89f65e2702ec7ec1d8a55b
Saket105 <57018899+Saket105@users.noreply.github.com>
2020-12-18 22:52:21 +0530

    12th commit

--- app/src/main/java/com/example/befit/ui/NotificationsFragment.java
+++ app/src/main/java/com/example/befit/ui/NotificationsFragment.java
@@ -1,10 +1,13 @@
 package com.example.befit.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,15 +15,36 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.befit.Activities.LoginActivity;
 import com.example.befit.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class NotificationsFragment extends Fragment {
 
-
+    Button logout;
+    FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
 
+        logout = root.findViewById(R.id.logout);
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
         return root;
+    }
+
+    private void logout() {
+        mAuth.signOut();
+        getActivity().finish();
+        startActivity(new Intent(getActivity(), LoginActivity.class));
+        Toast.makeText(getContext(), "Logged Out!", Toast.LENGTH_SHORT).show();
     }
 }
